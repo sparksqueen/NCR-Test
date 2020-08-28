4a9916dcb60eda14dda6608a69302ef912525f69
sparksqueen <61467494+sparksqueen@users.noreply.github.com>
2020-08-28 15:44:13 -0300

    Update Anagrama.js

--- 2.1.2 Anagrama/Anagrama.js
+++ 2.1.2 Anagrama/Anagrama.js
@@ -1,4 +1,7 @@
-
+/* 
+El algoritmo planteado esta hecho para ser ejecutado desde un navegador (posee prompt), con visibilidad de la consola.
+Posee comentarios para imprimir variables de debug.
+*/
 
 var CadenaA = prompt("Introduzca el texto a analizar");
 	CadenaA = CadenaA.toLowerCase();
@@ -8,14 +11,14 @@ var CadenaB = prompt("Introduzca las letras que desea comprobar si estan dentro 
 	LargoB= CadenaB.length;
 	
 
-var ArrayCadenaB= CadenaB.split('');
+var ArrayCadenaB= CadenaB.split(''); 	//separa la cadena en un array de caracteres
 var CantidadLetras = ArrayCadenaB.length;
 
 
 
 
-function permutaciones(PermutacionesDisponibles) { 
-	const _permutaciones = [];
+function permutaciones(PermutacionesDisponibles) { // esta funcion permite la permutacion de los caracteres y la creacion  
+	const _permutaciones = [];						//de un nuevo array(object) donde se encuentran almacenadas las variaciones disponibles
 
 	const permutar = (arr, permutacion=[]) => {
 		if(! arr.length){
@@ -30,14 +33,7 @@ function permutaciones(PermutacionesDisponibles) {
 			let sig = curr.splice(i, 1);
 
 			permutar(curr, permutacion.concat(sig)); 
-
-
-
-
-
 		}
-
- 
 	}
 
 	permutar(PermutacionesDisponibles);
@@ -46,59 +42,38 @@ function permutaciones(PermutacionesDisponibles) {
 
 
 
-
 var PermutacionesRealizadas = permutaciones(ArrayCadenaB);
-console.log(typeof PermutacionesRealizadas);
+//console.log( PermutacionesRealizadas);
 
  //PermutacionesRealizadas.forEach(object => object.join(""));
 
-for(i=0; i<PermutacionesRealizadas.length; i++){
+for(i=0; i<PermutacionesRealizadas.length; i++){			//une las permutaciones dentro del object como array de strings
 	 PermutacionesRealizadas[i] = PermutacionesRealizadas[i].join("");
 }
 
-var contador=0;
-var largoA = CadenaA.length;
 
-var n=0;
-var j=0;
+var contador=0;			//nueva creación de variables de apoyo, para ejecutar la busqueda en el algoritmo
+var LargoA = CadenaA.length;
 
-console.log(CadenaA.length);
+var n=0;  //indice de PermutacionesRealizadas
+var j=0;  //indice de busqueda de (sub) CadenaA
 
-	while(n<PermutacionesRealizadas.length)	{	
 
-		while(j<CadenaA.length){
+while(n < PermutacionesRealizadas.length){
 	
-			if(CadenaA.includes(PermutacionesRealizadas[n]) == false){ 
-				j=0;
-				break;
-			}else{
-			let CadenaPartida = CadenaA.substring(j);
-			j++;
-			console.log(CadenaPartida);
-				if(CadenaPartida.includes(PermutacionesRealizadas[n])){
-					 j = j + CadenaPartida.indexOf(PermutacionesRealizadas[n]) + CadenaB.length;
-					 contador++;
-					 console.log(contador);
-				if(CadenaPartida.length < CadenaA.length){
-								j = 0;
-								break;	
-							}	}
-	
-				 
-			}
-		}
+	let CadenaPartida = CadenaA.substring(j);
+
+	if(CadenaPartida.includes(PermutacionesRealizadas[n])){
+		contador++;
+		j = j + CadenaPartida.indexOf(PermutacionesRealizadas[n]) + CadenaB.length;
+	}else{
 		n++;
-		console.log(n);
-		console.log(j)
-
-
-
-
-
-
+		j=0;
+	}
 }
+
 if (CadenaA.length < CadenaB.length) {
-	document.write("No se puede analizar");
+	console.log("No se puede analizar");
 }else{ 
-	document.write("La palabra se encuentra contenida " + contador + " veces");
+	console.log("La palabra se encuentra contenida " + contador + " veces");
 }
