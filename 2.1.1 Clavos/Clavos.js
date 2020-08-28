ffe63d67bdcb8479da8f8975ce7b124eeb720518
sparksqueen <61467494+sparksqueen@users.noreply.github.com>
2020-08-28 02:22:24 -0300

    Update Clavos.js

--- 2.1.1 Clavos/Clavos.js
+++ 2.1.1 Clavos/Clavos.js
@@ -1,42 +1,38 @@
-
+/*
+El algoritmo planteado esta hecho para ser ejecutado desde la consola, o bien una terminal.
+*/
 var A = [1, 4, 5, 8];
-var B = [4, 6, 9, 10];
+var B = [4, 5, 9, 10];
 var C = [4, 6, 7, 10, 2];
 
-
-
 var N = 0; //recorre en pares A & B
-var final = C.length;
 var I = 0; //recorre C
 var contador = 0;
-var clavo = 0;
 
 
-while(I<final){
-        while(N<A.length && contador<A.length){
-            if(A[N] <= C[I] && C[I] <= B[N]) {
-                console.log("Clava");
+   while(I<C.length && contador < A.length){
+        while(N<A.length){
 
-                N++;
-                contador++;
-            }else{console.log("No Clava");
-                N++;
+            if(A[N] <= C[I] && C[I] <= B[N]){
+//           console.log("Clava");
+            contador++;
+
+//           }else{                          //Se deja comentado para revisar funcionamiento
+//           console.log("No Clava");
+            }
+            N++;      
             }
 
-   //     if(N>= 1) {
-    //       clavo++;
-      //      }
-        }
-        N=contador;
-        I++;
-        
-        }
-        if(contador==A.length){
-            console.log("Todos los clavos se han clavado con los primeros " + contador + " clavos");
+        N = contador;
+ //   console.log(N);
+    I++;
+    }
 
-        }else if(contador<A.length){
-            console.log("No se han podido clavar todos los clavos");
-        }
-    
-    
+    if(contador==A.length){
+    console.log("Todas las tablas se han clavado con los primeros " + I + " clavos.");
+    } else {
+        console.log("No se han podido clavar todos los clavos.")
+    }
+
+
 
